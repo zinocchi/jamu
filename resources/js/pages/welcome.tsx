3cecd6a18d60ff0b8f8217507a48930dd99f726f
Allghzl <milkymo9000@gmail.com>
2025-11-11 10:17:59 +0700

    initialize

---
+++ resources/js/pages/welcome.tsx
@@ -0,0 +1,85 @@
+import {
+    Card,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+import bgJamu from '@/lib/assets/Desain_tanpa_judul_upscayl_4x_ultramix-balanced-4x.png';
+import logoJamu from '@/lib/assets/Jamu!.png';
+import { Head } from '@inertiajs/react';
+import { HandCoins, ShoppingBag, Vegan } from 'lucide-react';
+
+export default function Welcome() {
+    return (
+        <div className="bg-[#f9f5f0]">
+            <div
+                className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
+                style={{ backgroundImage: `url(${bgJamu})` }}
+            >
+                <Head title="Jamu!" />
+                <div className="flex h-20 w-full items-center justify-between gap-4 bg-black/40 px-16 font-semibold text-white shadow-lg shadow-black/10 backdrop-blur-lg">
+                    <a href="">TESTIMONI</a>
+                    <a href="">PRODUK</a>
+                    <a href="/">
+                        <img
+                            src={logoJamu}
+                            alt="Logo Jamu!"
+                            className="h-32 w-auto"
+                        />
+                    </a>
+                    <a href="">TENTANG KAMI</a>
+                    <a href="">KONTAK</a>
+                </div>
+                <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center text-center text-white">
+                    <h1 className="mb-2 max-w-3xl font-Yusei text-2xl md:text-6xl">
+                        Jamu sehat dari dapur kami untuk keluarga anda
+                    </h1>
+                    <button className="mt-6 rounded bg-[#f87108] px-6 py-3 font-bold text-white shadow-lg shadow-[#f87108]/50 transition hover:bg-orange-600/90 hover:shadow-orange-600/40">
+                        CEK KESEHATAN ANDA
+                    </button>
+                </div>
+            </div>
+
+            <div className="mx-12 flex flex-row justify-between gap-8 py-24 text-center">
+                <Card className="w-xl border-none bg-white py-12 font-[Open_sans] text-[#f87108]">
+                    <HandCoins className="h-16 w-auto items-center justify-center text-[#f87108]" />
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-bold uppercase">
+                            harga bersahabat
+                        </CardTitle>
+                        <CardDescription className="mt-2 text-center text-[#575050]">
+                            Nikmati jamu sehat dengan harga ramah di kantong,
+                            cocok buat sehari-hari.
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
+
+                <Card className="w-xl border-none bg-white py-12 font-[Open_sans] text-[#f87108]">
+                    <Vegan className="h-16 w-auto items-center justify-center text-[#f87108]" />
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-bold uppercase">
+                            segar dan alami
+                        </CardTitle>
+                        <CardDescription className="mt-2 text-center text-[#575050]">
+                            Diracik langsung dari rempah pilihan, bikin tubuh
+                            makin fit dan berenergi.
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
+
+                <Card className="w-xl border-none bg-white py-12 font-[Open_sans] text-[#f87108]">
+                    <ShoppingBag className="h-16 w-auto items-center justify-center text-[#f87108]" />
+                    <CardHeader>
+                        <CardTitle className="text-2xl font-bold uppercase">
+                            varian lengkap
+                        </CardTitle>
+                        <CardDescription className="mt-2 text-center text-[#575050]">
+                            Dari kunyit asam sampai beras kencur, banyak pilihan
+                            rasa yang bisa nemenin gaya hidupmu.
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
+            </div>
+        </div>
+    );
+}
